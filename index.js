772a10b9d2672387891f8b6dd3b343d1b40e0f52
Sebastian Escobar <84691405+YummySalamy@users.noreply.github.com>
2023-07-28 21:55:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const FloatingButton = require('./FloatingButton');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FloatingButton from './FloatingButton';
 
-ReactDOM.render(
-  React.createElement(React.StrictMode, null, React.createElement(FloatingButton, null)),
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FloatingButton />
+  </React.StrictMode>,
+  document.getElementById('floating-button-root') 
 );
